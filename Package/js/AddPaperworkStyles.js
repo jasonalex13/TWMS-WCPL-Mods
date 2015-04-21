d52ba0f9eac40623186cec4b712a40ebc21d610e
jasonalex <info@jasonalex.net>
2015-04-21 12:22:39 +1000

    Optionally hide items that have not been rejected

--- Package/js/AddPaperworkStyles.js
+++ Package/js/AddPaperworkStyles.js
@@ -8,13 +8,55 @@
 //
 // VERSION		1.2
 
+//FUNCTION FOR SHOWING HIDING NON-REJECTED ITEMS
+
+function ShowOnlyRejectedFunction(checkval) {
+	if(checkval == true) {
+		$('table tr td:nth-child(8)').each(function(){
+		   if ($(this).text().trim() == '') {
+				$(this).parent().css('display','none');
+			}
+		}); 
+	} else if (checkval == false) {
+					$('table tr td:nth-child(8)').each(function(){
+		   if ($(this).text().trim() == '') {
+				$(this).parent().css('display','');
+			}
+		}); 
+	}
+}
 
 if(currentPageURL == urlencode("https://" + currentDomain + "/damstra/paperworkportal.asp") || currentPageURL == urlencode("https://www.damstra.com.au/damstra/paperworkportal.asp")) {
 	var PaperworkPriority = 0
 	
+	// if the paperwork priority can be found in a cookie, use it
 	if(getCookie("PaperworkPriority") != "")
 	{
 		document.getElementById("Priority").value = getCookie("PaperworkPriority");
+	}
+	
+	// add a button for hiding everything that isn't rejected
+	var RejectedCheckboxNode = document.createElement("label");
+	RejectedCheckboxNode.setAttribute("for","ShowOnlyRejected");
+	RejectedCheckboxNode.setAttribute("ID","ShowOnlyRejectedLabel");
+	document.getElementById("ViewBy").parentNode.appendChild(RejectedCheckboxNode);
+	document.getElementById("ShowOnlyRejectedLabel").innerHTML = "<input type='checkbox' value='0' name='ShowOnlyRejected' id='ShowOnlyRejected'> Show Only Rejected";
+	//when comeone clicks the rejected checkbox see what the change was and update the page accordingly
+	document.getElementById("ShowOnlyRejected").addEventListener('change',function(){
+		ShowOnlyRejectedValue = document.getElementById("ShowOnlyRejected").checked;
+		setCookie("ShowOnlyRejected",ShowOnlyRejectedValue,1);
+		ShowOnlyRejectedFunction(ShowOnlyRejectedValue);
+	});
+	
+	if(getCookie("ShowOnlyRejected") != "")
+	{
+		if(getCookie("ShowOnlyRejected") == "true"){
+			ShowOnlyRejectedValue = true;
+		} else if (getCookie("ShowOnlyRejected") == "false") {
+			ShowOnlyRejectedValue = false;
+		}
+		document.getElementById("ShowOnlyRejected").checked	 = ShowOnlyRejectedValue;
+		ShowOnlyRejectedFunction(JSON.parse(getCookie("ShowOnlyRejected")));
 	}
 	
 	addGlobalStyle('input[type="file"] {width:1000px;background-color:rgb(227,242,217);border:1px solid #7ac143; padding: 10px;}');
@@ -36,5 +78,5 @@ if(currentPageURL == urlencode("https://" + currentDomain + "/damstra/paperworkp
 	document.getElementById("Priority").addEventListener('change',function(){
 		PaperworkPriority = document.getElementById("Priority").value;
 		setCookie("PaperworkPriority",PaperworkPriority,1);
-		});
+	});
 }
