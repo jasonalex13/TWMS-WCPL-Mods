6a4a9fb54b294c3a368f094d9a733b99890f2779
jasonalex <info@jasonalex.net>
2015-04-24 11:21:49 +1000

    Create AddSkills.js

---
+++ Package/js/AddSkills.js
@@ -0,0 +1,10 @@
+// NAME						Profile Page Mods
+// DESCRIPTION		Adds Styles to the Employee Profile on Damstra TWMS for ease of use by WSF CURRAGH
+//
+// AUTHOR					Jason Hill <info@jasonalex.net>
+// COPYRIGHT			Copyright (C) 2015, by Jason Hill <info@jasonalex.net>
+//
+// PRE-REQ				base.js
+//
+// VERSION				1.2
+
