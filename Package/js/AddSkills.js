4ad23b6651c24003a1f4ebe1958050b18fbdd2e3
jasonalex <info@jasonalex.net>
2015-04-25 00:32:59 +1000

    all requested features now implemented and tested. should be right to merg

--- Package/js/AddSkills.js
+++ Package/js/AddSkills.js
@@ -6,20 +6,32 @@
 //
 // PRE-REQ				base.js
 //
-// VERSION				0.1
+// VERSION				0.19
 	
 if(currentPageURL.toLowerCase() == urlencode("https://" + currentDomain + "/damstra/include/panels/AddSkills.asp").toLowerCase() || currentPageURL == urlencode("https://www.damstra.com.au/damstra/paperworkportal.asp")) {
-	console.log("0.13")
-	var MineSelector = document.getElementById("MINE_ID")
-	var BeginDateField = document.getElementById("AS_OF")
+	console.log("0.19");
+	//just enable all the fields we want from the start
+	$("#NOTES").css('display','');
+	$("#AS_OF").css('display','');
+	$("#expdate").css('display','');
+	$("#skilllevel").css('display','');
+	$("#attach1").css('display','');
+	
+	var MineSelector = document.getElementById("MINE_ID");
+	var BeginDateField = document.getElementById("AS_OF");
+	var ExpDateField = document.getElementById("expdate");
 	
 	addGlobalStyle('input[type="file"] {width:400px;background-color:rgb(227,242,217);border:1px solid #7ac143; padding: 10px;}');
+	addGlobalStyle('#frm_skill > table > tbody > tr:nth-child(8) {display:none;}');
 	MineSelector.setAttribute("onchange","InitSkills(this.value);");
 	MineSelector.focus();
 	MineSelector.value = "WSF CURRAGH";
 	MineSelector.blur();
 	
-	BeginDateField.addEventListener("change",function(){
-		document.getElementById("skilllevel").value = "Competent"
+	ExpDateField.addEventListener("blur",function(){
+		document.getElementById("skilllevel").value = "Competent";
+	});
+	BeginDateField.addEventListener("blur",function(){
+		document.getElementById("skilllevel").value = "Competent";	
 	});
 }
