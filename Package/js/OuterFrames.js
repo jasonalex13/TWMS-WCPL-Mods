1401c3b695f6f36a5d5f94bcc39c6f45aea321bc
jasonalex <info@jasonalex.net>
2015-05-05 07:41:22 +1000

    Implement #10 and #9
    Adds "Curragh Mod" before the version number in the top left corner (#9)
    Uses the variable CurModAccentColor instead of being manually defined (#10 / #11)

--- Package/js/OuterFrames.js
+++ Package/js/OuterFrames.js
@@ -12,9 +12,9 @@ if(currentPageURL == urlencode("https://" + currentDomain + "/damstra/heading.as
 	var TWMSimgURL = chrome.extension.getURL("images/TWMS-heart.png");
 	$('img').prop('src', function(_,src) { return src.replace('https://' + currentDomain + '/damstra/images/DMSl.png', TWMSimgURL); });
 	addGlobalStyle("body {background-image: none; background-color:#333333;}");
-	addGlobalStyle("h3, h4 {color:#FF51B1;}");
+	addGlobalStyle("h3, h4 {color:"+ CurModAccentColor +";}");
 	
-	$('body').append("<div ID='backcontainer'><button id='backbutton' class='btn btn-mini btn-warning'>Back</button><button id='fwdbutton' class='btn btn-mini btn-info'>Fwd</button><span id='jlxVersion'>"+jlxVersioning()+"</span></div>");
+	$('body').append("<div ID='backcontainer'><button id='backbutton' class='btn btn-mini btn-warning'>Back</button><button id='fwdbutton' class='btn btn-mini btn-info'>Fwd</button><span id='jlxVersion'>Curragh Mod "+jlxVersioning()+"</span></div>");
 	addGlobalStyle("#backcontainer {position:absolute;top:0px;left:10px;}");
 	addGlobalStyle("#jlxVersion {color:#c4c4c4;font-size:10px;padding-left:5px;}");
 	
@@ -30,5 +30,5 @@ if(currentPageURL == urlencode("https://" + currentDomain + "/damstra/heading.as
 if(currentPageURL == urlencode("https://" + currentDomain + "/damstra/menu1.asp") || currentPageURL == urlencode("https://" + currentDomain + "/damstra/menu2.asp")  ) {
 	addGlobalStyle("body, #topDiv {background-image: none; background-color:#333333 !important;}");	
 	addGlobalStyle("li.nav-header {color:#ffffff !important;}");
-	addGlobalStyle("li a:link, li a:visited {color:#ffffff !important;} li a:hover, li a:active {background-color:#ff51b1 !important;color:#333333 !important;}")
+	addGlobalStyle("li a:link, li a:visited {color:#ffffff !important;} li a:hover, li a:active {background-color:"+ CurModAccentColor +" !important;color:#333333 !important;}")
 }
