9edbb1f5341bdbaf4f932ae696089578b407f591
jasonalex <info@jasonalex.net>
2015-05-11 20:41:36 +1000

    add initial option files

---
+++ Package/options/options.js
@@ -0,0 +1,44 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+  var EnableTheme = document.getElementById('EnableTheme').checked;
+  var AccentColor = document.getElementById('AccentColor').value;
+  var ThemeDL = document.getElementById('Theme').value;
+  var BackFwd = document.getElementById('BackFwd').checked;
+  var AgeCalc = document.getElementById('AgeCalc').checked;
+  chrome.storage.sync.set({
+    EnableTheme: EnableTheme,
+    AccentColor: AccentColor,
+	Theme: ThemeDL,
+	BackFwd: BackFwd,
+	AgeCalc: AgeCalc
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    EnableTheme: true,
+    AccentColor: '#ff51b1',
+	Theme: 'dark',
+	BackFwd: true,
+	AgeCalc: true
+  }, function(items) {
+    document.getElementById('EnableTheme').checked = items.EnableTheme;
+    document.getElementById('AccentColor').value = items.AccentColor;
+	document.getElementById('Theme').value = items.Theme;
+	document.getElementById('BackFwd').checked = items.BackFwd;
+	document.getElementById('AgeCalc').checked = items.AgeCalc;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
