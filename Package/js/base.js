c6956511f8c9461c9f13b6dbd600061e00667964
jasonalex <info@jasonalex.net>
2015-05-19 10:07:58 +1000

    Update version number v1.4.2

--- Package/js/base.js
+++ Package/js/base.js
@@ -4,13 +4,13 @@
 // AUTHOR		Jason Hill <info@jasonalex.net>
 // COPYRIGHT		Copyright (C) 2015, by Jason Hill <info@jasonalex.net>
 //
-// VERSION		1.4.1
+// VERSION		1.4.2
 
 callback = $.Callbacks();
 
 function jlxVersioning() {
 	// this is the overall version of the TWMS-Mod (per the manifest)
-	return "v1.4.1";
+	return "v1.4.2";
 }
 
 function shadeColor2(color, percent) {   
